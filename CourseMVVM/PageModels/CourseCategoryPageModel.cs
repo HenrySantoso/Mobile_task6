10455f915ba030f948ba80b87cc2270fa0db0608
HenrySantoso <119570803+HenrySantoso@users.noreply.github.com>
2024-12-05 22:48:29 +0700

    commit2

--- CourseMVVM/PageModels/CourseCategoryPageModel.cs
+++ CourseMVVM/PageModels/CourseCategoryPageModel.cs
@@ -3,6 +3,8 @@ using CommunityToolkit.Mvvm.Input;
 using CourseMVVM.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +13,34 @@ namespace CourseMVVM.PageModels
 {
     public partial class CourseCategoryPageModel : ObservableObject, IProjectTaskPageModel
     {
+        private readonly ICourseCategory _courseCategory;
         public IAsyncRelayCommand<ProjectTask> NavigateToTaskCommand => throw new NotImplementedException();
 
-        public bool IsBusy => throw new NotImplementedException();  
+        public bool IsBusy => throw new NotImplementedException();
+
+        public ObservableCollection<CourseCategory> CourseCategories { get; set; } = new();
+
+        public CourseCategoryPageModel(ICourseCategory courseCategory)
+        {
+            _courseCategory = courseCategory;
+        }
+
+        [RelayCommand]
+        async Task LoadCourseCategories()
+        {
+            try
+            {
+                CourseCategories.Clear();
+                var courseCategories = await _courseCategory.ListAsync();
+                foreach (var courseCategory in courseCategories)
+                {
+                    CourseCategories.Add(courseCategory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
     }
 }
